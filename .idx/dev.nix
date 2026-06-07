f8d30e0dcc9de2c65be94f18fb605f1a7ad6ac85
shonbufford06 <shonbufford06@gmail.com>
2026-06-07 05:18:06 +0000

    idz

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,10 +4,14 @@
     pkgs.nodejs_22
     pkgs.nodePackages.typescript-language-server
     pkgs.jdk
+    pkgs.nodePackages.tailwindcss-language-server
+    pkgs.npm
+    pkgs.yarn
   ];
   idx.extensions = [
     "esbenp.prettier-vscode"
     "dbaeumer.vscode-eslint"
+    "bradlc.vscode-tailwindcss"
   ];
   idx.previews = {
     enable = true;
